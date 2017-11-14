52812112c8c48ce9f6c4f3eefac7af755d02768c
Евгения Кривенко <jeka1206@mail.ru>
2017-11-14 22:53:30 +0200

    resize dashboard

--- src/client/ui/join_game.py
+++ src/client/ui/join_game.py
@@ -7,7 +7,7 @@ class Join(Frame):
     def __init__(self, master=None, **kw):
         Frame.__init__(self, master, **kw)
         self.create_widgets()
-        self.grid(row=0, column=0, padx=40, pady=40)
+        self.grid(row=0, column=3)
         self.nickname = None
 
     def get_attributes(self):
@@ -21,20 +21,18 @@ class Join(Frame):
         return (self.name, self.need, self.id)
 
     def create_widgets(self):
-        self._label = Label(self, text='Games', font='sans 16')
-        self._label.grid(row=0, columnspan=2, rowspan=2)
+        #self._label = Label(self, text='Choose the game')
+        #self._label.grid(row=0)
 
-        self._label = Label(self, text='')
-        self._label.grid(row=2)
         name_game, number, id = self.get_attributes()
 
         self._game_list = Listbox(self, height=5, selectmode=SINGLE, yscrollcommand=True, xscrollcommand=True)
         for i in range(len(name_game)):
             self._game_list.insert(i, 'Game: ' + name_game[i] + ' (' + str(number[i]) + ' players are needed)')
-        self._game_list.grid(row=3, ipadx=35, ipady=20)
+        self._game_list.grid(row=2, ipadx=100, pady=15, padx=10)
 
         self._button_join = Button(self, text='Join to game', command=self.join)
-        self._button_join.grid(row=4, pady=20)
+        self._button_join.grid(row=4, pady=10)
 
     def join(self):
         self.index = int(self.Game_list.curselection()[0])
